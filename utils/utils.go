b12fe2319714d9a30c8dd0d71a37720de3bbe658
Andrey <andrushk_nn@mail.ru>
2020-07-16 10:15:26 +0300

    кодировка ответа utf-8

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,6 @@ import (
 )
 
 func Respond(w http.ResponseWriter, data interface{})  {
-	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Content-Type", "application/json; charset=UTF-8")
 	json.NewEncoder(w).Encode(data)
 }
